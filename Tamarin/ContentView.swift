d799bfd3f2115898f19393df3181af73667b1548
Manav Bokinala <manav@mbokinala.com>
2026-08-27 18:20:31 -0700

    Initial Commit

---
+++ Tamarin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tamarin
+//
+//  Created by Manav Bokinala on 8/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tamarin/TamarinApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamarinApp.swift
+//  Tamarin
+//
+//  Created by Manav Bokinala on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct TamarinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
